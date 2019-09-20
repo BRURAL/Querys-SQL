918a72d417a089a6c8e0dbf7d7c50ed30e1eadeb
Brural <valeria@brasilrural.agr.br>
2019-09-20 16:09:46 -0300

    Atualização

--- QUERY SALES E CLIENTES.sql
+++ QUERY SALES E CLIENTES.sql
@@ -47,7 +47,7 @@ alter table RELATORIO alter column cidade varchar(50);
 alter table RELATORIO alter column uf varchar(50);
 alter table RELATORIO alter column representantelegal varchar(50);
 alter table RELATORIO alter column tipoatendimento varchar(100);
-alter table RELATORIO alter column motivoagendamento varchar(100);
+alter table RELATORIO alter column motivoatendimento varchar(100);
 alter table RELATORIO alter column mailing varchar(100);
 alter table RELATORIO alter column campanha varchar(100);
 alter table RELATORIO alter column datacadastro date;
@@ -55,23 +55,86 @@ alter table RELATORIO alter column horacadastro date;
 alter table RELATORIO alter column observacao varchar(max);
 
 --RETIRAR DUPLICATAS DA PLANILHA SALESCALL
+create procedure sp_converterSales_RetirarDuplicidades
+as begin
+alter table RELATORIO alter column codigo int not null;
+alter table RELATORIO alter column operador varchar(100);
+alter table RELATORIO alter column nome varchar(100);
+alter table RELATORIO alter column cpfcnpj varchar(50);
+alter table RELATORIO alter column telefone1 varchar(50);
+alter table RELATORIO alter column telefone2 varchar(50);
+alter table RELATORIO alter column telefone3 varchar(50);
+alter table RELATORIO alter column telefone4 varchar(50);
+alter table RELATORIO alter column telefone5 varchar(50);
+alter table RELATORIO alter column cidade varchar(50);
+alter table RELATORIO alter column uf varchar(50);
+alter table RELATORIO alter column representantelegal varchar(50);
+alter table RELATORIO alter column tipoatendimento varchar(100);
+alter table RELATORIO alter column motivoatendimento varchar(100);
+alter table RELATORIO alter column mailing varchar(100);
+alter table RELATORIO alter column campanha varchar(100);
+alter table RELATORIO alter column datacadastro date;
+alter table RELATORIO alter column horacadastro date;
+alter table RELATORIO alter column observacao varchar(max);
+
 
 insert into relatorio
-select * from relatorio2
+select * from relatorio_DES;
 
-delete from relatorio
-where codigo in
-	(select max(codigo)from RELATORIO
-	group by nome
-	having count(nome)>1)
+DELETE FROM RELATORIO WHERE CODIGO NOT IN (SELECT MAX(CODIGO) As IdMaximo FROM RELATORIO GROUP BY Nome);
 
 DELETE FROM RELATORIO 
-WHERE MAILING = 'TESTE'
+WHERE MAILING = 'TESTE';
+end
 
-select p.* from atualizada p
-where 
+
+
+
+
+------------APENAS EXECUTAR INDIVIDUALMENTE CADA UMA DAS 4 LINHAS ABAIXO PARA ATUALIZAR A TABELA SALESCALL
+
+exec sp_converterSales_RetirarDuplicidades
+
+ALTER TABLE RELATORIO ADD ID INT IDENTITY;
+
+DELETE FROM RELATORIO WHERE ID NOT IN (SELECT MAX(ID) As IdMaximo FROM RELATORIO GROUP BY Nome);
+
+ALTER TABLE RELATORIO DROP COLUMN ID;
+
+--TESTANDO DUPLICIDADE
+
+select COUNT(NOME), NOME from RELATORIO
+	group by nome
+	having count(nome)>1
+
+
+
 
 --SELECT PARA MOSTRAR QUAL COMPRADOR TEM COMPRA E LIGAÇÃO NA SALESCALL
-
 SELECT r.* FROM RELATORIO R
-INNER JOIN atualizada R1 ON R1.razao_social != R.nome
+INNER JOIN atualizada R1 ON R1.razao_social = R.nome
+
+
+--SELECT PARA MOSTRAR QUAL COMPRADOR CONTÉM NA SALES E NÃO NA CLIENTE
+SELECT R.* FROM RELATORIO R
+left outer JOIN ATUALIZADA R1 ON (R.NOME = R1.RAZAO_SOCIAL)
+where R1.razao_social is null
+
+
+--SELECT PARA MOSTRAR QUAL COMPRADOR CONTÉM CLIENTES E NÃO SALES
+SELECT * FROM ATUALIZADA R1
+WHERE razao_social NOT IN (SELECT nome FROM RELATORIO)
+order by razao_social
+
+
+
+
+
+SELECT * FROM RELATORIO
+WHERE NOME = 'SANTOS CORDEIRO LUIZ'
+
+NILCEU SOMAVILLA
+HELOISA GABRIELA DE OLIVEIRA
+SANTOS CORDEIRO LUIZ
+MARCO AURELIO DA SILVA RAMOS
+EUGENIO ANTONIO ROLING
